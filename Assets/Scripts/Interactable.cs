9c7a498a0dc91c8f2ea3a8524db768268b913506
grant norris <>
2019-10-06 17:37:21 +0100

    Started adding exit tiles.

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    BoxCollider2D col;
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Player") {
+            Debug.Log("collided with player");
+        }
+    }
+}
