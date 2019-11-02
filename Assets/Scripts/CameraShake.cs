b84e036fc84240997ec35c7d5e7da01b0ca3db23
grant norris <>
2019-11-02 15:23:59 +0000

    Increase chromatic aberration on camera shake

--- Assets/Scripts/CameraShake.cs
+++ Assets/Scripts/CameraShake.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class CameraShake : MonoBehaviour
 {
     public static CameraShake instance;
+    public Material lofiShader;
 
     void Awake() {
         if (instance == null) {
@@ -16,12 +17,22 @@ public class CameraShake : MonoBehaviour
         Vector3 originPos = transform.position;
         float elapsed     = 0f;
 
+        Vector2 chromaticAberration = lofiShader.GetVector("Vector2_6B378F0B");
+
+        if (lofiShader != null) {
+            lofiShader.SetVector("Vector2_6B378F0B", new Vector2(.0025f, 0));
+        }
+
         while (duration > elapsed) {
             float xPos = Random.Range(-1f, 1f) * magnitude;
             transform.localPosition = new Vector3(originPos.x + xPos, originPos.y, originPos.z);
             elapsed += Time.deltaTime;
 
             yield return null;
+        }
+
+        if (lofiShader != null) {
+            lofiShader.SetVector("Vector2_6B378F0B", chromaticAberration);
         }
 
         transform.localPosition = originPos;
