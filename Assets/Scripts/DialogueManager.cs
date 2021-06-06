ef3b5e2f67e06d4923b247be87ed5e62d73ffc85
grant norris <grant_n@live.co.uk>
2021-06-06 21:47:42 +0100

    Slowed typing of dialogue letters.

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -49,7 +49,7 @@ public class DialogueManager : MonoBehaviour
 
         foreach (char letter in sentence.ToCharArray()) {
             DialogueText.text += letter;
-            yield return null;
+            yield return new WaitForSeconds(0.025f);
         }
     }
 
