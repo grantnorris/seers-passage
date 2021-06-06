8010c0af1b37e5a43cc9a180045ed9d478330122
grant norris <grant_n@live.co.uk>
2021-06-06 14:27:24 +0100

    Started adding dialogue system.

---
+++ Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager instance;
+
+    public GameObject dialogueBox;
+    public Text DialogueText;
+
+    Queue<string> sentences = new Queue<string>();
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        }
+    }
+
+    // Start dialogue
+    public void StartDialogue(Dialogue dialogue) {
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences) {
+            sentences.Enqueue(sentence.ToUpper());
+        }
+
+        DisplayNextSentence();
+    }
+
+    // Display next sentence
+    public void DisplayNextSentence() {
+        if (sentences.Count == 0) {
+            EndDialogue();
+            return;
+        }
+
+        dialogueBox.SetActive(true);
+
+        string sentence = sentences.Dequeue();
+
+        DialogueText.text = sentence;
+    }
+
+    // End dialogue
+    void EndDialogue() {
+        Debug.Log("end dialogue");
+        dialogueBox.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (DialogueManager.instance == null) {
+            return;
+        }
+
+        DialogueManager.instance.StartDialogue(dialogue);
+    }
+}
