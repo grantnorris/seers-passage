2ce3452b25c5cc1a8b0fdfcee7589d1b07b36899
grant norris <grant_n@live.co.uk>
2021-06-09 20:52:09 +0100

    Health icons now update to reflect the player's health

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,7 +1,10 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public GameObject UI;
+
     int health = 3;
 
     // The current health of the player
@@ -12,11 +15,41 @@ public class PlayerHealth : MonoBehaviour
     public void ReduceHealth() {
         health--;
 
-        Debug.Log("player health = " + health);
+        UpdateHealthUI();
 
         if (health <= 0) {
             Die();
         }
+    }
+
+    // Update health UI icon display
+    void UpdateHealthUI() {
+        if (UI == null) {
+            return;
+        }
+
+        Image img = null;
+
+        switch (health)
+        {
+        case 2:
+            img = UI.transform.GetChild(0).GetComponent<Image>();
+            break;
+        case 1:
+            img = UI.transform.GetChild(1).GetComponent<Image>();
+            break;
+        case 0:
+            img = UI.transform.GetChild(2).GetComponent<Image>();
+            break;
+        }
+
+        if (img == null) {
+            return;
+        }
+
+        Color color = img.color;
+        color.a = .5f;
+        img.color = color;
     }
     
     // End the game 
