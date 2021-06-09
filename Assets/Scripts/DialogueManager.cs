2297ecbc261d57c2b77aa7fa26e923e81b1ddc44
grant norris <grant_n@live.co.uk>
2021-06-09 19:49:12 +0100

    Dialogue adjustments for different types. Installed textmeshpro package.

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -2,14 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogueManager : MonoBehaviour
 {
     public static DialogueManager instance;
 
-    public GameObject dialogueBox;
-    public Text DialogueText;
+    public GameObject dialogueUI;
     public DialogueType[] types;
+
+    GameObject dialogueBox;
 
     Queue<string> sentences = new Queue<string>();
 
@@ -17,12 +19,15 @@ public class DialogueManager : MonoBehaviour
         if (instance == null) {
             instance = this;
         }
+
+        dialogueUI.SetActive(false);
     }
 
     // Start dialogue
     public void StartDialogue(Dialogue dialogue) {
         sentences.Clear();
         GameManager.instance.DisablePlayerControl();
+        Destroy(dialogueBox);
 
         foreach (string sentence in dialogue.sentences) {
             sentences.Enqueue(sentence.ToUpper());
@@ -33,14 +38,28 @@ public class DialogueManager : MonoBehaviour
         foreach (DialogueType type in types) {
             if (type.name == dialogue.type) {
                 dialogueType = type;
+
+                if (type.dialogueBoxPrefab != null && dialogueUI != null) {
+                    dialogueBox = Instantiate(type.dialogueBoxPrefab);
+                    dialogueBox.transform.SetParent(dialogueUI.transform, false);
+                }
+
                 break;
             }
         }
 
-        if (dialogueType != null) {
-            dialogueBox.GetComponent<Image>().sprite = dialogueType.ui;
+        if (dialogueBox == null) {
+            EndDialogue();
+            return;
         }
 
+        StartCoroutine("WaitAndStartDialogue");
+    }
+
+    // Wait for a small amount of time and then start the dialogue
+    IEnumerator WaitAndStartDialogue() {
+        yield return new WaitForSeconds(.5f);
+        dialogueUI.SetActive(true);
         DisplayNextSentence();
     }
 
@@ -59,17 +78,28 @@ public class DialogueManager : MonoBehaviour
 
     // Type sentence into dialogue box
     IEnumerator TypeSentence(string sentence) {
-        DialogueText.text = "";
+        TMP_Text textUi = dialogueBox.GetComponentInChildren<TMP_Text>(true);
+
+        if (textUi == null) {
+            Debug.Log("no text ui found");
+            yield break;
+        }
+
+        textUi.text = "";
 
         foreach (char letter in sentence.ToCharArray()) {
-            DialogueText.text += letter;
+            textUi.text += letter;
             yield return new WaitForSeconds(0.025f);
         }
     }
 
     // End dialogue
     void EndDialogue() {
-        dialogueBox.SetActive(false);
+        if (dialogueBox != null) {
+            Destroy(dialogueBox);
+        }
+
+        dialogueUI.SetActive(false);
         GameManager.instance.EnablePlayerControl();
     }
 }
@@ -78,4 +108,5 @@ public class DialogueManager : MonoBehaviour
 public class DialogueType {
     public string name;
     public Sprite ui;
+    public GameObject dialogueBoxPrefab;
 }
