9829efc02eb644a8e17e0217bc0ba00cbbb792df
grant norris <grant_n@live.co.uk>
2021-07-03 21:00:33 +0100

    Started adding key interactable.

---
+++ Assets/Scripts/Interactable/KeyInteractable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyInteractable : Interactable
+{
+    public override void Interact() {
+        Debug.Log("KEY!!");
+    }
+}
