21eff9c598b9089795c3c579a5a46761e77aedc9
grant norris <grant_n@live.co.uk>
2021-08-14 18:13:25 +0100

    Build map script color mappings are now manually defined in script vs using public list.

--- Assets/Scripts/BuildMap.cs
+++ Assets/Scripts/BuildMap.cs
@@ -12,7 +12,7 @@ public class BuildMap : MonoBehaviour
     [HideInInspector]
     public UnityEvent mapBuilt;
     public Sprite map;
-    public ColorToTile[] colorMappings;
+    [HideInInspector]
     public List<TileByLocation> tiles = new List<TileByLocation>();
     Texture2D mapTex;
 
@@ -40,19 +40,69 @@ public class BuildMap : MonoBehaviour
     }
 
     ColorToTile[] ColorMappings() {
-        return colorMappings;
-        // return new ColorToTile[] {
-        //     new ColorToTile(
-        //         "floor",
-        //         new Color32(255, 255, 255, 1),
-        //         Resources.Load<GameObject>("Prefabs/Floor")
-        //     ),
-        //     new ColorToTile(
-        //         "wall",
-        //         new Color32(0, 0, 0, 1),
-        //         Resources.Load<GameObject>("Prefabs/Wall Tile")
-        //     )
-        // };
+        // return colorMappings;
+        return new ColorToTile[] {
+            new ColorToTile(
+                "Floor",
+                new Color(1, 1, 1, 1),
+                Resources.Load<GameObject>("Prefabs/Floor")
+            ),
+            new ColorToTile(
+                "Wall",
+                new Color(0, 0, 0, 1),
+                Resources.Load<GameObject>("Prefabs/Wall")
+            ),
+            new ColorToTile(
+                "Player Spawn",
+                new Color(0.4352941f, 0.7450981f, 0.2666667f, 1),
+                Resources.Load<GameObject>("Prefabs/Player Spawn")
+            ),
+            new ColorToTile(
+                "Surround",
+                new Color(0.9294118f, 0.1411765f, 0.5647059f, 1),
+                Resources.Load<GameObject>("Prefabs/Surround")
+            ),
+            new ColorToTile(
+                "Exit",
+                new Color(0.7450981f, 0.2666667f, 0.2666667f, 1),
+                Resources.Load<GameObject>("Prefabs/Exit")
+            ),
+            new ColorToTile(
+                "Light Spawn",
+                new Color(0f, 0.8235294f, 1f, 1),
+                Resources.Load<GameObject>("Prefabs/Light Spawn")
+            ),
+            new ColorToTile(
+                "Switch",
+                new Color(1f, 0.8470588f, 0f, 1),
+                Resources.Load<GameObject>("Prefabs/Switch")
+            ),
+            new ColorToTile(
+                "Gate",
+                new Color(1f, 0.6352941f, 0f, 1),
+                Resources.Load<GameObject>("Prefabs/Gate")
+            ),
+            new ColorToTile(
+                "Entrance",
+                new Color(0.3529412f, 1f, 0f, 1),
+                Resources.Load<GameObject>("Prefabs/Entrance")
+            ),
+            new ColorToTile(
+                "Journal",
+                new Color(0f, 0.4705882f, 1f, 1),
+                Resources.Load<GameObject>("Prefabs/Journal")
+            ),
+            new ColorToTile(
+                "Key Pickup",
+                new Color(0.4941176f, 0f, 1f, 1),
+                Resources.Load<GameObject>("Prefabs/Key Pickup")
+            ),
+            new ColorToTile(
+                "Gate Locked",
+                new Color(0.4039216f, 0.2901961f, 0.2f, 1),
+                Resources.Load<GameObject>("Prefabs/Gate Locked")
+            )
+        };
     }
 
     Texture2D texFromSprite() {
