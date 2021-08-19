153733df376a1d55f641ce99917080b66071c64e
grant norris <grant_n@live.co.uk>
2021-08-19 11:47:07 +0100

    Continued adding script for floor details.

--- Assets/Scripts/FloorTileDisplay.cs
+++ Assets/Scripts/FloorTileDisplay.cs
@@ -4,64 +4,185 @@ using UnityEngine;
 
 public class FloorTileDisplay : TileDisplay
 {
-    public List<Sprite> topDetailSprites = new List<Sprite>();
-    public bool hasDetails;
+    [SerializeField]
+    List<Sprite> topDetailSprites = new List<Sprite>();
+    [SerializeField]
+    List<Sprite> rightDetailSprites = new List<Sprite>();
+    [SerializeField]
+    List<Sprite> downDetailSprites = new List<Sprite>();
+    [SerializeField]
+    List<Sprite> leftDetailSprites = new List<Sprite>();
+    [HideInInspector]
+    public bool hasTopDetails;
+    [HideInInspector]
+    public bool hasRightDetails;
+    [HideInInspector]
+    public bool hasBottomDetails;
+    [HideInInspector]
+    public bool hasLeftDetails;
 
     public override void Initialise() {
         base.Initialise();
+        AssignDetails();
         CreateDetails();
+        StartCoroutine(CreateDetails());
     }
 
-    void CreateDetails() {
-        if (topDetailSprites.Count == 0) {
-            return;
-        }
-
-        // if (Random.Range(0, 100) <= 50)  {
+    // Determine which details should be present, if any
+    void AssignDetails() {
+        if (Random.Range(0, 100) <= 25)  {
             for (int i = 0; i < 4; i++) {
                 GameObject adjacentTile = null;
-                float tileRotation = 0f;
+                List<Sprite> sprites = new List<Sprite>();
                 
                 switch (i)
                 {
                     // Up
                     case 0:
                     adjacentTile = tileUp;
-                    tileRotation = 0f;
+                    sprites = topDetailSprites;
                     break;
 
                     // Right
                     case 1:
                     adjacentTile = tileRight;
-                    tileRotation = -90f;
+                    sprites = rightDetailSprites;
                     break;
 
                     // Down
                     case 2:
                     adjacentTile = tileDown;
-                    tileRotation = -180f;
+                    sprites = downDetailSprites;
                     break;
 
                     // Left
                     case 3:
                     adjacentTile = tileLeft;
-                    tileRotation = -270f;
+                    sprites = leftDetailSprites;
                     break;
                 }
 
-                if (adjacentTile == null || adjacentTile.tag != "Wall") {
+                if (sprites.Count == 0 || adjacentTile == null || adjacentTile.tag != "Wall") {
+                    continue;
+                }    
+            
+                switch (i)
+                {
+                    // Up
+                    case 0:
+                    hasTopDetails = true;
+                    break;
+
+                    // Right
+                    case 1:
+                    hasRightDetails = true;
+                    break;
+
+                    // Down
+                    case 2:
+                    hasBottomDetails = true;
+                    break;
+
+                    // Left
+                    case 3:
+                    hasLeftDetails = true;
+                    break;
+                }            
+            }
+        }
+    }
+
+    IEnumerator CreateDetails() {
+        yield return null;
+        FloorTileDisplay tileUpDisplay = tileUp.GetComponent<FloorTileDisplay>();
+        FloorTileDisplay tileRightDisplay = tileRight.GetComponent<FloorTileDisplay>();
+        FloorTileDisplay tileDownDisplay = tileDown.GetComponent<FloorTileDisplay>();
+        FloorTileDisplay tileLeftDisplay = tileLeft.GetComponent<FloorTileDisplay>();
+
+        for (int i = 0; i < 4; i++) {
+            Sprite sprite = null;
+
+            switch (i)
+            {
+                // Up
+                case 0:
+                if (!hasTopDetails) {
                     continue;
                 }
 
-                GameObject detail = new GameObject();
-                Transform detailTransform = detail.transform;
-                detail.name = "Floor Detail";
-                detailTransform.SetParent(transform);
-                detail.transform.localPosition = Vector3.zero;
-                detail.transform.eulerAngles = new Vector3(0, 0, tileRotation);
-                Sprite sprite = topDetailSprites[Random.Range(0, topDetailSprites.Count - 1)];
-                detail.AddComponent<SpriteRenderer>().sprite = sprite;
+                if (tileLeftDisplay != null && tileLeftDisplay.hasTopDetails && tileRightDisplay != null && tileRightDisplay.hasTopDetails) {
+                    sprite = topDetailSprites[2];
+                } else if (tileLeftDisplay != null && tileLeftDisplay.hasTopDetails) {
+                    sprite = topDetailSprites[3];
+                } else if (tileRightDisplay != null && tileRightDisplay.hasTopDetails) {
+                    sprite = topDetailSprites[1];
+                } else {
+                    sprite = topDetailSprites[0];
+                }
+                break;
+
+                // Right
+                case 1:
+                if (!hasRightDetails) {
+                    break;
+                }
+
+                if (tileUpDisplay != null && tileUpDisplay.hasRightDetails && tileDownDisplay != null && tileDownDisplay.hasRightDetails) {
+                    sprite = rightDetailSprites[2];
+                } else if (tileDownDisplay != null && tileDownDisplay.hasRightDetails) {
+                    sprite = rightDetailSprites[3];
+                } else if (tileUpDisplay != null && tileUpDisplay.hasRightDetails) {
+                    sprite = rightDetailSprites[1];
+                } else {
+                    sprite = rightDetailSprites[0];
+                }
+                break;
+
+                // Down
+                case 2:
+                if (!hasBottomDetails) {
+                    break;
+                }
+
+                if (tileLeftDisplay != null && tileLeftDisplay.hasBottomDetails && tileRightDisplay != null && tileRightDisplay.hasBottomDetails) {
+                    sprite = downDetailSprites[2];
+                } else if (tileLeftDisplay != null && tileLeftDisplay.hasBottomDetails) {
+                    sprite = downDetailSprites[3];
+                } else if (tileRightDisplay != null && tileRightDisplay.hasBottomDetails) {
+                    sprite = downDetailSprites[1];
+                } else {
+                    sprite = downDetailSprites[0];
+                }
+                break;
+
+                // Left
+                case 3:
+                if (!hasLeftDetails) {
+                    break;
+                }
+
+                if (tileUpDisplay != null && tileUpDisplay.hasLeftDetails && tileDownDisplay != null && tileDownDisplay.hasLeftDetails) {
+                    sprite = leftDetailSprites[2];
+                } else if (tileDownDisplay != null && tileDownDisplay.hasLeftDetails) {
+                    sprite = leftDetailSprites[3];
+                } else if (tileUpDisplay != null && tileUpDisplay.hasLeftDetails) {
+                    sprite = leftDetailSprites[1];
+                } else {
+                    sprite = leftDetailSprites[0];
+                }
+                break;
             }
-        // }
+
+            if (sprite == null) {
+                continue;
+            }
+
+            GameObject detail = new GameObject();
+            Transform detailTransform = detail.transform;
+            detail.name = "Floor Detail";
+            detailTransform.SetParent(transform);
+            detailTransform.localPosition = Vector3.zero;
+            detail.AddComponent<SpriteRenderer>().sprite = sprite;
+        }
     }
 }
