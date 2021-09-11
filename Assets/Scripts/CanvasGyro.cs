ecbcc01b54050375b95d8b801518d131069c0324
grant norris <grant_n@live.co.uk>
2021-09-11 11:42:46 +0100

    Added normalising, smoothing and clamping for gyro parallax.

--- Assets/Scripts/CanvasGyro.cs
+++ Assets/Scripts/CanvasGyro.cs
@@ -6,7 +6,11 @@ public class CanvasGyro : MonoBehaviour
 {
     Gyroscope gyro;
     RectTransform rect;
-    float movement = 100f;
+    float movement = 50f;
+    float maxOffset = 5f;
+    float restingAttitude = 0f;
+    float velocity;
+    float curOffset = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -19,14 +23,33 @@ public class CanvasGyro : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Normalise();
+        SetOffset();
+    }
+
+    // Gradually recenters the UI based on the current attitude
+    void Normalise() {
+        restingAttitude = Mathf.SmoothDamp(restingAttitude, gyro.attitude.y * movement, ref velocity, .05f);
+    }
+
+    // Offset rect transform based on current attitude
+    void SetOffset() {
         if (rect == null) {
             return;
         }
 
-        rect.offsetMin = new Vector2(gyro.attitude.y * movement, 0);
-        rect.offsetMax = new Vector2(gyro.attitude.y * movement, 0);
+        float offset = gyro.attitude.y * movement;
 
-        Debug.Log("input.gyro.attitude: " + gyro.attitude);
+        if (offset < 0 && restingAttitude < 0) {
+            offset -= restingAttitude;
+        } else {
+            offset += restingAttitude;
+        }
 
+        curOffset = Mathf.SmoothDamp(curOffset, Mathf.Clamp(offset, -maxOffset, maxOffset), ref velocity, .15f);
+        Debug.Log("curoffset = " + curOffset);
+
+        rect.offsetMin = new Vector2(curOffset, 0);
+        rect.offsetMax = new Vector2(curOffset, 0);
     }
 }
