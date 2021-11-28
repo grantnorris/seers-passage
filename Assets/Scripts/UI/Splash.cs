25f60484a17b204d1878eaac162cb4be59a330ee
grant norris <grant_n@live.co.uk>
2021-11-28 14:20:06 +0000

    Added simple splash screen out animation.

--- Assets/Scripts/UI/Splash.cs
+++ Assets/Scripts/UI/Splash.cs
@@ -3,6 +3,16 @@ using UnityEngine.SceneManagement;
 
 public class Splash : MonoBehaviour
 {
+    Animator anim;
+
+    void Start() {
+        anim = GetComponent<Animator>();
+    }
+    
+    public void TransitionOut() {
+        anim.SetTrigger("transitionOut");
+    }
+
     public void ChangeScene() {
         SceneManager.LoadScene("Level Select");
     }
