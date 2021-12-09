5cc8d538931db2189dd8d58abfb823c80c217361
grant norris <grant_n@live.co.uk>
2021-12-09 20:53:40 +0000

    Removed debug log.

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -91,8 +91,6 @@ public class AudioManager : MonoBehaviour
             return;
         }
 
-        print("play theme " + sound.name);
-
         StartCoroutine(FadeSounds(theme, sound, fadeSpeed));
         theme = sound;
     }
